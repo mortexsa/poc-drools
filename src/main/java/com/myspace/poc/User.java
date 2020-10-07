ff419dd42c5cfe2ed86ae107d8df617cba56b778
admin <>
2020-10-07 14:44:06 +0000

    {/src/main/java/com/myspace/poc/User.java}

--- src/main/java/com/myspace/poc/User.java
+++ src/main/java/com/myspace/poc/User.java
@@ -7,29 +7,37 @@ package com.myspace.poc;
 @javax.persistence.Entity
 public class User implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(generator = "USER_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "USER_ID_SEQ", name = "USER_ID_GENERATOR")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(generator = "USER_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(sequenceName = "USER_ID_SEQ", name = "USER_ID_GENERATOR")
+	private java.lang.Long id;
 
-    public User() {
-    }
-    
-    public User(java.lang.Long id) {
-        this.id = id;
-    }
+	private java.lang.String name;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	public User() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public User(java.lang.Long id, java.lang.String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 }
